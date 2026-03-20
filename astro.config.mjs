4a16f770b093ad95ea9efc5c6d2180b6548dd7e2
loecedas <loecedas@gmail.com>
2026-03-20 22:23:48 +0800

    fix(deploy): add missing astro dependency and make adapter logic robust for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+const adapter = (process.env.NETLIFY) 
+  ? netlify() 
+  : vercel();
+
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter,
   integrations: [
     tailwind(),
     react(),
