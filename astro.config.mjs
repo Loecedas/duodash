345b77bbba94a7d0defa6d64618c6faa0a392c92
loecedas <loecedas@gmail.com>
2026-03-21 19:18:15 +0800

    revert: remove Cloudflare Workers config for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import cloudflare from "@astrojs/cloudflare";
-
 const adapter = (process.env.NETLIFY) 
   ? netlify() 
   : vercel();
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter,
   integrations: [
     tailwind(),
     react(),
